955deb398033bc187bd1a004bb01e11530cb1daa
DELL <DELL@DELL-PC>
2019-07-01 15:17:08 +0530

    Add project files.

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       
+        &nbsp; EMP NAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       
+        <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+        <br />
+        <br />
+        &nbsp;EMP MO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server" ></asp:TextBox>
+        <br />
+        <br />
+        EMP DEP&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server" ></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="view" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="innsert" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delete" ToolTip="to delete record just insert name of employee" />
+&nbsp;&nbsp;
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="CLear" />
+        <br />
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+                con.Open();
+                SqlCommand scmd;
+                SqlDataReader dat;
+                string outpt = " ";
+
+                string sq = "select * from employee";
+
+                scmd = new SqlCommand(sq, con);
+
+                dat = scmd.ExecuteReader();
+
+                while (dat.Read())
+                {
+                    outpt = outpt + dat.GetValue(1) + "-" + dat.GetValue(2) + "-" + dat.GetValue(3) + "</br>";
+                }
+                Response.Write(outpt);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+                con.Open();
+
+                string insertQuery = "insert into employee(Ename,Emob,Dept)values (@ename,@emob,@dept)";
+                SqlCommand cmd = new SqlCommand(insertQuery, con);
+                cmd.Parameters.AddWithValue("@ename", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@emob", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@dept", TextBox3.Text);
+
+                cmd.ExecuteNonQuery();
+
+                Response.Write("Student inseted Successfully!!!thank you");
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox3.Text = "";
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+                con.Open();
+                String del = "delete  from employee where Ename= '" + TextBox1.Text +"'";
+                SqlCommand cmd = new SqlCommand(del, con);
+                cmd.ExecuteNonQuery();
+
+                Response.Write("Deleted.");
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox3.Text = "";
+
+            }
+            catch(Exception ex)
+            { 
+                Response.Write(ex.Message);
+            }
+            }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+                con.Open();
+                String up = "update employee set Dept= '" + TextBox3.Text + "'where Ename='" + TextBox1.Text +"'";
+                SqlCommand cmd = new SqlCommand(up, con);
+                cmd.ExecuteNonQuery();
+
+                Response.Write("updated.");
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox3.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+        }
+    }
+}
+
